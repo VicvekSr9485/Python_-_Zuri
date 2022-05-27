6baa3bd9f3c7e8738d88be58428b93397c88b5c5
VicvekSr <Olamideoso65@gmail.com>
2022-05-27 20:18:44 +0000

    This program reads text file

--- Reading_Text_Files/main.py
+++ Reading_Text_Files/main.py
@@ -1,13 +1,32 @@
-def read_file_content(story):
-    with open("story.txt") as f:
-        contents = f.read()
-        print(contents)
-    read_file_content()
+# Read text from a file, and count the occurence of words in that text
+# Example:
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+
+def read_file_content(filename):
+   with open('/workspace/Python_-_Zuri/Reading_Text_Files/story.txt') as file:
+    contents = file.read()
+    print(contents)
     return "Hello World"
 
 
-#def count_words():
-   # text = read_file_content("./story.txt")
-    # [assignment] Add your code here
+def count_words():
+    text = open("/workspace/Python_-_Zuri/Reading_Text_Files/story.txt", "r")
+    d = dict()
+    for line in text:
+	    line = line.strip()
+	    line = line.lower()
+	    words = line.split(" ")
+	    for word in words:
+		    if word in d:
+			    d[word] = d[word] + 1
+		    else:
+			    d[word] = 1
+    for key in list(d.keys()):
+	    print(key, ":", d[key])
+    
+   
+#return {"as": 10, "would": 20}
 
-   # return {"as": 10, "would": 20}
+read_file_content("./story.txt")
+count_words()
